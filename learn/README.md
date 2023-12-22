ec95b8c9b07a304b7595ea5e2ce5254d1b0360d6
lile <44667481+lilecloud@users.noreply.github.com>
2023-12-22 12:05:47 +0000

    add

---
+++ learn/README.md
@@ -0,0 +1,13 @@
+这个是想白嫖github的codespace 为了防止关机之后所有的东西丢失，所以讲所有步骤记录下来
+
+
+
+#### 依赖
+- k8s环境，codespace 自带了minikube 只有用启动minikube 就可以嘞  
+```sh minikube start  ```
+- 安装istio  
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.1 TARGET_ARCH=x86_64 sh -  
+export PATH=/workspaces/istio-1.20.1/bin:$PATH
+
+
+
---
+++ learn/init.sh
@@ -0,0 +1,19 @@
+cd /workspaces/ && \
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.1 TARGET_ARCH=x86_64 sh -  && \
+export PATH=/workspaces/istio-1.20.1/bin:$PATH &&\
+istioctl install --set profile=demo -y && \
+kubectl label namespace default istio-injection=enable && \
+
+
+
+# 以下为记录的命令
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+
+# 进入minikubenode
+minikube ssh --node=<node-name>
+
+
+
+
+
+
